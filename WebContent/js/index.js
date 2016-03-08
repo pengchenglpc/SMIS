320c3cab5b7ea84fce4649273c54a4b1115d7999
pengchenglpc <pengchenglpc@gmail.com>
2016-03-08 15:15:41 +0800

    完善菜单管理功能

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,19 +1,44 @@
 (function($){
 	$(function(){
-		$('#sys_tree').tree({
-			url:'sys/findAllMenu.action',
-			onClick:function(node){
-				var tab = $('#center_tab').tabs('getTab', node.text);
-				if(tab){
-					$('#center_tab').tabs('select', node.text);
-				}else{
-					$('#center_tab').tabs('add',{    
-					    title:node.text,    
-					    href:node.menuUrl,
-					    closable:true   
-					});  
+		$('#nav_accordion').accordion({
+			onSelect:function(title, index){
+				//面板选中时加载该Panel菜单项目数据
+				var panel = $('#nav_accordion').accordion('getPanel', index);
+				panel.children('ul').tree({
+					url:'sys/findParentMenu.action',
+					queryParams:{'menu.menuParent':panel.attr('id')},
+					onClick:function(node){
+						var tab = $('#center_tab').tabs('getTab', node.text);
+						if(tab){
+							$('#center_tab').tabs('select', node.text);
+						}else{
+							$('#center_tab').tabs('add',{
+							    title:node.text,    
+							    href:node.menuUrl,
+							    closable:true   
+							});  
+						}
+					}
+				});
+			}
+		});
+		$.ajax('sys/findRootMenu.action',{
+			dataType:'json',
+			async:false,
+			success:function(data){
+				if(data){
+					for(var i = 0; i < data.length; i++){
+						var menu = data[i];
+						$('#nav_accordion').accordion('add', {
+							id:menu.id,
+							title: menu.menuName,
+							content: '<ul></ul>',
+							selected: i === 0 ? true : false
+						});
+					}
 				}
 			}
 		});
+
 	});
 })(jQuery);
--- WebContent/js/system/menuList.js
+++ WebContent/js/system/menuList.js
@@ -1,112 +1,145 @@
 (function($){
-	var treeObj = $.fn.zTree.getZTreeObj("menu_manage_tree"),
-		node = treeObj.getSelectedNodes()[0];
-	$('#menu_datagrid').treegrid({
-		method:'GET',
-		url:'sys/findAllMenu.action',
-		fit:true,
-		singleSelect:false,
-		rownumbers:true,
-		fitColumns:true,
-		//striped:false,
-		toolbar:'#menu_toolbar',
-		idField:'menuId',
-		treeField:'menuName',
-		/*pagination:true,
-		pageNumber:1,
-		pageSize:10,
-		pageList:[5,10,20,30,50,100],*/
-		columns:[[{
-			field:'menuId',
-			checkbox:true
-		},{
-			title:'菜单名称',
-			width:100,
-			field:'menuName'
-		},{
-			title:'英文名称',
-			width:100,
-			field:'menuEnglish'
-		},{
-			title:'菜单层级',
-			width:70,
-			field:'menuLevel'
-		},{
-			title:'URL',
-			width:150,
-			field:'actionUrl'
-		},{
-			title:'是否有效',
-			width:50,
-			field:'isActive',
-			formatter:function(value){
-				if(value == 0){
-					return '有效';
-				}else{
-					return '无效';
-				}
-			}
-		},{
-			title:'排序值',
-			field:'sort'
-		},{
-			title:'描述',
-			width:200,
-			field:'description'
-		}]]
+	$(function(){
+		$('#menu_datagrid').treegrid({
+			url:'sys/findTreeMenu.action',
+			rownumbers: true,
+			toolbar:[{
+				text:'添加',
+				iconCls:'icon-add',
+				handler:addMenu
+			},'-',{
+				text:'修改',
+				iconCls:'icon-edit',
+				handler:edit
+			},'-',{
+				text:'删除',
+				iconCls:'icon-remove',
+				handler:remove
+			},'-',{
+				text:'刷新',
+				iconCls:'icon-reload',
+				handler:refresh
+			}],
+			idField:'id',
+			treeField:'menuName',
+			height:500,
+			//rownumbers:true,
+			columns:[[
+			          {field:'id',checkbox:true},
+			          {field:'menuName',title:'菜单名称',width:100},    
+			          {field:'menuUrl',title:'菜单路径',width:100},
+			          {field:'available', title:'是否可用', width:100,formatter:function(value){
+			        	  if(value === 0){
+			        		  return '不可用';
+			        	  }else if(value === 1){
+			        		  return '可用';
+			        	  }else{
+			        		  return '无效值';
+			        	  }
+			          }},
+			          {field:'createTime', title:'创建时间', width:120},
+			          {field:'updateTime', title:'最后修改时间', width:120},
+			          {field:'description',title:'菜单描述',width:250}
+			      ]] 
+		});
 	});
-//	$('#menu_datagrid').treegrid({
-//		url:'sys/findAllMenu.action',
-//		height:329,
-//		//pagination:true,
-//		toolbar:[{
-//			text:'添加',
-//			iconCls:'icon-add',
-//			handler:addMenu
-//		},'-',{
-//			text:'修改',
-//			iconCls:'icon-edit'
-//		},'-',{
-//			text:'删除',
-//			iconCls:'icon-remove'
-//		},'-',{
-//			text:'刷新',
-//			iconCls:'icon-reload'
-//		}],
-//		queryParams:{
-//			'menu.menuParent':node['id']
-//		},
-//		idField:'id',
-//		treeField:'menuName',
-//		rownumbers:true,
-//		columns:[[
-//		          {field:'id',checkbox:true},
-//		          {field:'menuName',title:'菜单名称',width:100},    
-//		          {field:'menuUrl',title:'菜单路径',width:100},
-//		          {field:'available', title:'是否可用', width:100,formatter:function(value){
-//		        	  if(value === 0){
-//		        		  return '不可用';
-//		        	  }else if(value === 1){
-//		        		  return '可用';
-//		        	  }else{
-//		        		  return '无效值';
-//		        	  }
-//		          }},
-//		          {field:'createTime', title:'创建时间', width:120},
-//		          {field:'updateTime', title:'最后修改时间', width:120},    
-//		          {field:'description',title:'菜单描述',width:250}
-//		      ]] 
-//	});
-	function addMenu(){
-		menuDialog('添加菜单');
+	function edit(){
+		var row = $('#menu_datagrid').treegrid('getSelected');
+		if(!row){
+			$.messager.alert('警告','请选择您要修改的菜单项');
+			return;
+		}
+		var dialog = menuDialog('修改菜单',function(){
+			$('#menu_form').form('submit', {
+				url:'sys/editMenu.action',
+				success:function(data){
+					data = $.parseJSON(data);
+					if(data['status'] === 1){
+						$('#menu_datagrid').treegrid('reload');
+						dialog.dialog('destroy');
+					}
+				}
+			});
+		}, function(){
+			var _row = {};
+			for(var key in row){
+				_row['menu.' + key] = row[key];
+			}
+			$('#menu_form').form('load',_row);
+			//$('#menu_form').find('label[name="menu.parent"]').html(row['menuName']);
+		});
 	}
-	function menuDialog(title, fn){
+	function remove(){
+		var row = $('#menu_datagrid').treegrid('getSelected');
+		if(!row){
+			$.messager.alert('警告','请选择您要删除的菜单项');
+			return;
+		}
+		if(row['children']){
+			$.messager.alert('警告','该菜单项包含有子菜单，请先删除子菜单');
+			return;
+		}
+		$.messager.confirm('确认','您确定要删除该菜单项吗？',function(r){    
+		    if (r){    
+		        $.get('sys/removeMenu.action', {'menu.id':row['id']}, function(data){
+		        	if(data['status'] === 0){
+		        		$.messager.alert('警告','菜单项删除失败');
+		        		return;
+		        	}
+		        	refresh();
+		        }, 'json');  
+		    }    
+		});  
+	}
+	function refresh(){
+		$('#menu_datagrid').treegrid('reload');
+	}
+	function addMenu(){
+		var row = $('#menu_datagrid').treegrid('getSelected');
+		if(row && row['leaf'] === 0){
+			$.messager.alert('警告','该菜单项不能添加子节点');   
+			return;
+		}
+		var dialog = menuDialog('添加菜单',function(){
+			$('#menu_form').form('submit', {
+				url:'sys/addMenu.action',
+				success:function(data){
+					data = $.parseJSON(data);
+					if(data['status'] === 1){
+						$('#menu_datagrid').treegrid('reload');
+						dialog.dialog('destroy');
+					}
+				}
+			});
+		}, function(){
+			if(row){
+				$('#menu_form').form('load',{
+					'menu.menuParent':row['id']
+				});
+				$('#menu_form').find('label[name="menu.parent"]').html(row['menuName']);
+			}
+		});
+	}
+	function menuDialog(title, fn, load){
 		var dialog = $('<div>').dialog({
 			title:title,
 			modal:true,
-			width:400,
+			width:600,
 			href:'system/menu.html',
-			height:300
+			height:260,
+			onLoad:load,
+			buttons:[{
+				text:'确定',
+				width:50,
+				handler:fn
+			},{
+				text:'取消',
+				handler:function(){
+					dialog.dialog('destroy');
+				},
+				width:50
+			}]
 		});
+		return dialog;
 	}
 })(jQuery);
